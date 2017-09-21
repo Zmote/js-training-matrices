689145b40ac6c55827032dbfbba4c68bab26f38b
DESKTOP-3M9COAN\Zmote <shodaime.hikarikage@gmail.com>
2017-09-21 03:15:19 +0200

    all version, including map reduce for matrice multiplication

--- js/functions.js
+++ js/functions.js
@@ -6,10 +6,10 @@
 "use strict";
 
 function execute(sourceMatrix, targetMatrix, operation) {
-    var newMatrix = [];
-    for (var i = 0; i < sourceMatrix.length; i++) {
+    const newMatrix = [];
+    for (let i = 0; i < sourceMatrix.length; i++) {
         newMatrix.push([]);
-        for (var j = 0; j < sourceMatrix[i].length; j++) {
+        for (let j = 0; j < sourceMatrix[i].length; j++) {
             newMatrix[i].push(operation(sourceMatrix[i][j], targetMatrix[i][j]));
         }
     }
@@ -32,114 +32,124 @@ Array.prototype.substract = function(mx) {
     return execute(this, mx, subtract);
 };
 
+/*******************************************/
+/**********ITERATIVE SOLUTIONS*************/
+/*****************************************/
+
 // First iteration
-// Array.prototype.multiply = function (mx) {
-//     var firstMatrix = [[],[],[]];
-//     var i = 0;
-//     var j = 0;
-//     for(i = 0; i < this.length; i++){
-//         for(j = 0; j < this[i].length; j++){
-//             // firstMatrix[i].push(this[i][0]*mx[0][j]);
-//             firstMatrix[i].push("a" + i + "0 * " + "b" + 0 + j);
-//         }
-//     }
-//     console.log(firstMatrix);
-//     var secondMatrix = [[],[],[]];
-//     for(i = 0; i < this.length; i++){
-//         for(j = 0; j < this[i].length; j++){
-//             // secondMatrix[i].push(this[i][1]*mx[1][j]);
-//             secondMatrix[i].push("a" + i + "1" + "*" + "b1" + j)
-//         }
-//     }
-//     console.log(secondMatrix);
-//     var thirdMatrix = [[],[],[]];
-//     for(i = 0; i < this.length; i++){
-//         for(j = 0; j < this[i].length; j++){
-//             // thirdMatrix[i].push(this[i][2]*mx[2][j]);
-//             thirdMatrix[i].push("a" + i + "2 * " + "b" + "2" + j);
-//         }
-//     }
-//     console.log(thirdMatrix);
-//     return firstMatrix.add(secondMatrix).add(thirdMatrix);
-// };
+Array.prototype.multiply_v1 = function (mx) {
+    const firstMatrix = [[], [], []];
+    let i = 0;
+    let j = 0;
+    for(i = 0; i < this.length; i++){
+        for(j = 0; j < this[i].length; j++){
+            firstMatrix[i].push(this[i][0]*mx[0][j]);
+            // firstMatrix[i].push("a" + i + "0 * " + "b" + 0 + j);
+        }
+    }
+    // console.log(firstMatrix);
+    const secondMatrix = [[], [], []];
+    for(i = 0; i < this.length; i++){
+        for(j = 0; j < this[i].length; j++){
+            secondMatrix[i].push(this[i][1]*mx[1][j]);
+            // secondMatrix[i].push("a" + i + "1" + "*" + "b1" + j)
+        }
+    }
+    // console.log(secondMatrix);
+    const thirdMatrix = [[], [], []];
+    for(i = 0; i < this.length; i++){
+        for(j = 0; j < this[i].length; j++){
+            thirdMatrix[i].push(this[i][2]*mx[2][j]);
+            // thirdMatrix[i].push("a" + i + "2 * " + "b" + "2" + j);
+        }
+    }
+    // console.log(thirdMatrix);
+    return firstMatrix.add(secondMatrix).add(thirdMatrix);
+};
 
 //Second iteration: This iteration summarizes a lot, but has a lot of static parts as well
-// Array.prototype.multiply = function (mx) {
-//     var product = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
-//
-//     for (var h = 0; h < this.length; h++) {
-//         var partialMatrix = [[], [], []];
-//         for (var i = 0; i < this.length; i++) {
-//             for (var j = 0; j < this[i].length; j++) {
-//                 partialMatrix[i].push(this[i][h] * mx[h][j]);
-//             }
-//         }
-//         product = product.add(partialMatrix);
-//     }
-//     return product;
-// };
+Array.prototype.multiply_v2 = function (mx) {
+    let product = [[0, 0, 0], [0, 0, 0], [0, 0, 0]];
+
+    for (let h = 0; h < this.length; h++) {
+        const partialMatrix = [[], [], []];
+        for (let i = 0; i < this.length; i++) {
+            for (let j = 0; j < this[i].length; j++) {
+                partialMatrix[i].push(this[i][h] * mx[h][j]);
+            }
+        }
+        product = product.add(partialMatrix);
+    }
+    return product;
+};
 
 //third iteration: This iteration has a more dynamic code (dynamic lenghts etc.) + every function does something specific
 //which helps to keep overview over the parts of the code
-// function partialSum(product, partialMatrix) {
-//     if (product !== null) {
-//         product = product.add(partialMatrix);
-//     } else {
-//         product = partialMatrix;
-//     }
-//     return product;
-// }
-//
-// function calculateColumns(i, partialMatrix, h, mx) {
-//     for (var j = 0; j < this[i].length; j++) {
-//         partialMatrix[i].push(this[i][h] * mx[h][j]);
-//     }
-//     return j;
-// }
-// function calculateRows(partialMatrix, h, mx) {
-//     for (var i = 0; i < this.length; i++) {
-//         partialMatrix.push([]);
-//         calculateColumns.call(this, i, partialMatrix, h, mx);
-//     }
-//     return i;
-// }
-//
-// function summarizePartialProducts(mx, product) {
-//     for (var h = 0; h < this.length; h++) {
-//         var partialMatrix = [];
-//         calculateRows.call(this, partialMatrix, h, mx);
-//         product = partialSum(product, partialMatrix);
-//     }
-//     return product;
-// }
-//
-// Array.prototype.multiply = function (mx) {
-//     return summarizePartialProducts.call(this, mx, null);
-// };
-
-//Fourth iteration: We keep optimizing, make the code more readable, optimize routines where we can
-function partialSum(product, partialMatrix) {
-    return product !== null ? product.add(partialMatrix) : partialMatrix;
-}
-function calculateColumns(row, partialMatrix, dimension, mx) {
-    partialMatrix.push([]);
-    for (var col = 0; col < this[row].length; col++) {
-        partialMatrix[row].push(this[row][dimension] * mx[dimension][col]);
-    }
-}
-function calculateRows(partialMatrix, dimension, mx) {
-    for (var row = 0; row < this.length; row++) {
-        calculateColumns.call(this, row, partialMatrix, dimension, mx);
-    }
-    return partialMatrix;
-}
-function summarizePartialProducts(mx) {
-    var product = null;
-    for (var dimension = 0; dimension < this.length; dimension++) {
-        product = partialSum(product, calculateRows.call(this, [], dimension, mx));
+function partialSum_v3(product, partialMatrix) {
+    if (product !== null) {
+        product = product.add(partialMatrix);
+    } else {
+        product = partialMatrix;
     }
     return product;
 }
+
+function calculateColumns_v3(i, partialMatrix, h, mx) {
+    let j;
+    for (j = 0; j < this[i].length; j++) {
+        partialMatrix[i].push(this[i][h] * mx[h][j]);
+    }
+    return j;
+}
+function calculateRows_v3(partialMatrix, h, mx) {
+    let i;
+    for (i = 0; i < this.length; i++) {
+        partialMatrix.push([]);
+        calculateColumns_v3.call(this, i, partialMatrix, h, mx);
+    }
+    return i;
+}
+
+function summarizePartialProducts_v3(mx, product) {
+    for (let h = 0; h < this.length; h++) {
+        const partialMatrix = [];
+        calculateRows_v3.call(this, partialMatrix, h, mx);
+        product = partialSum_v3(product, partialMatrix);
+    }
+    return product;
+}
+
+Array.prototype.multiply_v3 = function (mx) {
+    return summarizePartialProducts_v3.call(this, mx, null);
+};
+
+//Fourth iteration: We keep optimizing, make the code more readable, optimize routines where we can
+function partialSum_v4(product, partialMatrix) {
+    return product !== null ? product.add(partialMatrix) : partialMatrix;
+}
+function calculateColumns_v4(row, partialMatrix, dimension, mx) {
+    partialMatrix.push([]);
+    for (let col = 0; col < this[row].length; col++) {
+        partialMatrix[row].push(this[row][dimension] * mx[dimension][col]);
+    }
+}
+function calculateRows_v4(partialMatrix, dimension, mx) {
+    for (let row = 0; row < this.length; row++) {
+        calculateColumns_v4.call(this, row, partialMatrix, dimension, mx);
+    }
+    return partialMatrix;
+}
+function summarizePartialProducts_v4(mx) {
+    let product = null;
+    for (let dimension = 0; dimension < this.length; dimension++) {
+        product = partialSum_v4(product, calculateRows_v4.call(this, [], dimension, mx));
+    }
+    return product;
+}
+
+Array.prototype.multiply_v4 = function (mx) {
+    return summarizePartialProducts_v4.call(this, mx);
+};
 
 //Fith iteration: What could be next? For example, refactoring all the helper codes into own file!
 //That's basically the spirit, how do we make the code simpler, more manageable, more performant etc.
@@ -149,8 +159,53 @@ function summarizePartialProducts(mx) {
 // 3)optimize performance
 // 4) Start over
 
-Array.prototype.multiply = function (mx) {
-    return summarizePartialProducts.call(this, mx);
+/********************************************/
+/**********MAP REDUCE SOLUTIONS*************/
+/******************************************/
+
+function reverseRowsAndColumns(matrix) {
+    let transformedMx = [];
+    matrix.map((row) => {
+        row.map((val, cIndex) => {
+        if (!transformedMx[cIndex]) transformedMx.push([]);
+    transformedMx[cIndex].push(val);
+});
+});
+    return transformedMx;
+}
+
+// First iteration: map reduce solution
+Array.prototype.multiply_v5 = function (mx) {
+    let transformedMx = reverseRowsAndColumns(mx);
+    return this.map((row)=>{
+            return row.map((rowValue, rowIndex)=>{
+                return transformedMx[rowIndex].map((columnValue,columnIndex)=>{
+                    return row[columnIndex] * columnValue;
+}).reduce((acc, val)=>{
+        return acc + val;
+});
+});
+});
+};
+
+//Second iteration: map reduce solution
+function gatherRowColumnProducts(mx, rowIndex, row) {
+    return reverseRowsAndColumns(mx)[rowIndex].map((columnValue, columnIndex) => {
+        return row[columnIndex] * columnValue;
+    });
+}
+function calculateProductRow(factorRow, factorMatrice) {
+    return factorRow.map((rowValue, rowIndex) => {
+        return gatherRowColumnProducts(factorMatrice, rowIndex, factorRow)
+            .reduce((accumulatedValue, currentValue) => {
+                return accumulatedValue + currentValue;
+            });
+    });
+}
+Array.prototype.multiply_v6 = function (factorMatrice) {
+    return this.map((factorRow)=>{
+        return calculateProductRow(factorRow, factorMatrice);
+    });
 };
 
 Array.prototype.display = function () {
--- js/main.js
+++ js/main.js
@@ -59,11 +59,21 @@ matrix1.add(matrix2).display();
 // 02: substract and display
 matrix1.substract(matrix2).display();
 
-// 03: multiply and display, 3x3 matrices
-matrix1.multiply(matrix2).display();
+console.log("MULTIPLICATION VARIATIONS START");
 
+// 03: multiply and display, 3x3 matrices
+matrix1.multiply_v1(matrix2).display();
+matrix1.multiply_v2(matrix2).display();
+matrix1.multiply_v3(matrix2).display();
+matrix1.multiply_v4(matrix2).display();
+matrix1.multiply_v5(matrix2).display();
+matrix1.multiply_v6(matrix2).display();
+
+console.log("MULTIPLICATION VARIATIONS END");
+
+//These are just examples, haven't checked if they produce correct 4 by 4 matrices, or 5 by 5 matrices
 //04: 4x4 matrices
-matrix3.multiply(matrix4).display();
+matrix3.multiply_v6(matrix4).display();
 
 //05: 5x5 matrices
-matrix5.multiply(matrix6).display();
+matrix5.multiply_v6(matrix6).display();
